ea055c865a58a6a6848670b6e7d0ce51700812b3
leftbrain stuff <leftbrainstuff@gmail.com>
2022-11-18 22:22:27 +1030

    Address aws-ssm not appearing in menu

--- .templates/aws-ssm/build.py
+++ .templates/aws-ssm/build.py
@@ -6,7 +6,20 @@ haltOnErrors = True
 
 # Main wrapper function. Required to make local vars work correctly
 def main():
+  import subprocess
+  global dockerComposeServicesYaml # The loaded memory YAML of all checked services
+  global toRun # Switch for which function to run when executed
+  global buildHooks # Where to place the options menu result
   global currentServiceName # Name of the current service
+  global issues # Returned issues dict
+  global haltOnErrors # Turn on to allow erroring
+
+  from deps.consts import servicesDirectory, templatesDirectory, volumesDirectory, servicesFileName
+
+  # runtime vars
+  serviceVolume = volumesDirectory + currentServiceName # Unused in example
+  serviceService = servicesDirectory + currentServiceName # Unused in example
+  serviceTemplate = templatesDirectory + currentServiceName
 
   # This lets the menu know whether to put " >> Options " or not
   # This function is REQUIRED.
@@ -45,7 +58,30 @@ def main():
       return buildHooks
     return buildHooks
 
-  # Entrypoint for execution
+  # This service will not check anything unless this is set
+  # This function is optional, and will run each time the menu is rendered
+  def runChecks():
+    checkForIssues()
+    return []
+
+  # This function is optional, and will run after the docker-compose.yml file is written to disk.
+  def postBuild():
+    return True
+
+  # This function is optional, and will run just before the build docker-compose.yml code.
+  def preBuild():
+    execComm = "bash {currentServiceTemplate}/build.sh".format(currentServiceTemplate=serviceTemplate) # --- You may want to change this
+    print("[aws-ssm]: ", execComm) # --- Ensure to update the service name with yours
+    subprocess.call(execComm, shell=True) # This is where the magic happens
+    return True
+
+  # #####################################
+  # Supporting functions below
+  # #####################################
+
+  def checkForIssues():
+    return True
+
   if haltOnErrors:
     eval(toRun)()
   else:
@@ -56,7 +92,7 @@ def main():
 
 # This check isn't required, but placed here for debugging purposes
 global currentServiceName # Name of the current service
-if currentServiceName == 'aws-ssm': # Make sure you update this.
+if currentServiceName == 'aws-ssm': # --- Ensure to update the service name with yours
   main()
 else:
-  print("Error. '{}' Tried to run 'aws-ssm' config".format(currentServiceName))
+  print("Error. '{}' Tried to run 'aws-ssm' config".format(currentServiceName)) # --- Ensure to update the service name with yours
