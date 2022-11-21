995a73293475423b0a9744a2af91d8c71ea2f3db
Ian Falconer <38296760+leftbrainstuff@users.noreply.github.com>
2022-11-21 17:47:06 +1030

    Update install-webmin.sh
    Update with dependencies

--- scripts/install-webmin.sh
+++ scripts/install-webmin.sh
@@ -7,6 +7,10 @@
 # Download
 wget http://prdownloads.sourceforge.net/webadmin/webmin_2.000_all.deb
 
+# Update Dependencies
+sudo apt --fix-broken
+apt-get install perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl apt-show-versions python unzip shared-mime-info
+
 # Install
 ## Run as sudo
 dpkg -i webmin_2.000_all.deb
