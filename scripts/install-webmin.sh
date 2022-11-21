6b661ab1c61b169b5b4319a7b93949cdf6250e3f
Ian Falconer <38296760+leftbrainstuff@users.noreply.github.com>
2022-11-21 17:17:02 +1030

    Create install-webmin.sh
    Create install script

---
+++ scripts/install-webmin.sh
@@ -0,0 +1,11 @@
+!#/bin/bash/
+
+# Webmin is a lightweight gui for headless environments.
+## from https://www.webmin.com/deb.html
+## from https://github.com/webmin/webmin#installation
+
+# Download
+wget http://prdownloads.sourceforge.net/webadmin/webmin_2.000_all.deb
+
+# Install
+dpkg -i webmin_2.000_all.deb
