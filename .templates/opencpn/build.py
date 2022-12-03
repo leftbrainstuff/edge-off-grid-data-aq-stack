67e72676ed54bb8a0ffa777b7682dcd22ecc44bd
leftbrain stuff <leftbrainstuff@gmail.com>
2022-12-03 21:08:15 +1030

    Resolve merge conflicts

--- .templates/opencpn/build.py
+++ .templates/opencpn/build.py
@@ -1,50 +1,37 @@
 #!/usr/bin/env python3
 
-<<<<<<< HEAD
-=======
-# Be warned that globals and variable scopes do not function normally in this Python script. This is because this script is eval'd with exec.
-
->>>>>>> e248047 (Create build.py)
 issues = {} # Returned issues dict
 buildHooks = {} # Options, and others hooks
 haltOnErrors = True
 
 # Main wrapper function. Required to make local vars work correctly
 def main():
-<<<<<<< HEAD
-  import subprocess
-=======
-  from blessed import Terminal
-  from deps.chars import specialChars, commonTopBorder, commonBottomBorder, commonEmptyLine # Common functions used when creating menu
-  import types
+  import os
   import time
+  from blessed import Terminal
+  from deps.chars import specialChars, commonTopBorder, commonBottomBorder, commonEmptyLine, padText
+  from deps.consts import servicesDirectory, templatesDirectory, volumesDirectory
+  from deps.common_functions import getExternalPorts, getInternalPorts, checkPortConflicts, enterPortNumberWithWhiptail
 
->>>>>>> e248047 (Create build.py)
   global dockerComposeServicesYaml # The loaded memory YAML of all checked services
   global toRun # Switch for which function to run when executed
   global buildHooks # Where to place the options menu result
   global currentServiceName # Name of the current service
   global issues # Returned issues dict
   global haltOnErrors # Turn on to allow erroring
-<<<<<<< HEAD
-
-  from deps.consts import servicesDirectory, templatesDirectory, volumesDirectory, servicesFileName
 
   # runtime vars
-  serviceVolume = volumesDirectory + currentServiceName # Unused in example
-  serviceService = servicesDirectory + currentServiceName # Unused in example
+  portConflicts = []
+  serviceVolume = volumesDirectory + currentServiceName
+  serviceService = servicesDirectory + currentServiceName
   serviceTemplate = templatesDirectory + currentServiceName
-=======
-  global hideHelpText
 
-  try: # If not already set, then set it to prevent errors.
+  try: # If not already set, then set it.
     hideHelpText = hideHelpText
   except:
     hideHelpText = False
 
-  # runtime vars
-  portConflicts = []
->>>>>>> e248047 (Create build.py)
+  documentationHint = 'https://sensorsiot.github.io/IOTstack/Containers/Opencpn' #TODO redirect
 
   # This lets the menu know whether to put " >> Options " or not
   # This function is REQUIRED.
@@ -89,26 +76,12 @@ def main():
     checkForIssues()
     return []
 
-<<<<<<< HEAD
-=======
-  # This is the menu that will run for " >> Options "
-  def runOptionsMenu():
-    menuEntryPoint()
-    return True
-
->>>>>>> e248047 (Create build.py)
   # This function is optional, and will run after the docker-compose.yml file is written to disk.
   def postBuild():
     return True
 
   # This function is optional, and will run just before the build docker-compose.yml code.
   def preBuild():
-<<<<<<< HEAD
-    execComm = "bash {currentServiceTemplate}/build.sh".format(currentServiceTemplate=serviceTemplate) # --- You may want to change this
-    print("[opencpn]: ", execComm) # --- Ensure to update the service name with yours
-    subprocess.call(execComm, shell=True) # This is where the magic happens
-=======
->>>>>>> e248047 (Create build.py)
     return True
 
   # #####################################
@@ -116,86 +89,17 @@ def main():
   # #####################################
 
   def checkForIssues():
-<<<<<<< HEAD
-    return True
-
-=======
     for (index, serviceName) in enumerate(dockerComposeServicesYaml):
       if not currentServiceName == serviceName: # Skip self
-        currentServicePorts = getExternalPorts(currentServiceName)
-        portConflicts = checkPortConflicts(serviceName, currentServicePorts)
+        currentServicePorts = getExternalPorts(currentServiceName, dockerComposeServicesYaml)
+        portConflicts = checkPortConflicts(serviceName, currentServicePorts, dockerComposeServicesYaml)
         if (len(portConflicts) > 0):
           issues["portConflicts"] = portConflicts
 
-  def getExternalPorts(serviceName):
-    externalPorts = []
-    try:
-      yamlService = dockerComposeServicesYaml[serviceName]
-      if "ports" in yamlService:
-        for (index, port) in enumerate(yamlService["ports"]):
-          try:
-            externalAndInternal = port.split(":")
-            externalPorts.append(externalAndInternal[0])
-          except:
-            pass
-    except:
-      pass
-    return externalPorts
+  ############################
+  # Menu Logic
+  ############################
 
-  def checkPortConflicts(serviceName, currentPorts):
-    portConflicts = []
-    if not currentServiceName == serviceName:
-      yamlService = dockerComposeServicesYaml[serviceName]
-      servicePorts = getExternalPorts(serviceName)
-      for (index, servicePort) in enumerate(servicePorts):
-        for (index, currentPort) in enumerate(currentPorts):
-          if (servicePort == currentPort):
-            portConflicts.append([servicePort, serviceName])
-    return portConflicts
-
-
-
-  # #####################################
-  # Example menu below
-  # #####################################
-  # You can build your menu system any way you like. This one is provided as an example.
-  # Checkout Blessed for full functionality, like text entry and so on at: https://blessed.readthedocs.io/en/latest/
-
-  # The functions the menu executes are below. They must be placed before the menu list 'menuItemsExample'
-  def menuCmdItem1():
-    print("You chose item1!")
-    return True
-
-  def menuCmdAnotherItem():
-    print("This is another menu item")
-    return True
-
-  def nop():
-    return True
-
-  def menuCmdStillAnotherItem():
-    print("This is still another menu item")
-    return True
-
-  def goBack():
-    global selectionInProgress
-    selectionInProgress = False
-    return True
-
-  # The actual menu
-  menuItemsExample = [
-    ["Item 1", menuCmdItem1],
-    ["Another item", menuCmdAnotherItem],
-    ["I'm skipped!", nop, { "skip": True }],
-    ["Still another item", menuCmdStillAnotherItem],
-    ["Error item"],
-    ["Error item"],
-    ["Some custom thing", nop, { "customProperty": True }],
-    ["I'm also skipped!", nop, { "skip": True }],
-    ["Go back", goBack]
-  ]
-
-  # Vars that the menu uses
   global currentMenuItemIndex
   global selectionInProgress
   global menuNavigateDirection
@@ -204,68 +108,128 @@ def main():
   selectionInProgress = True
   currentMenuItemIndex = 0
   menuNavigateDirection = 0
-  needsRender = True
+  needsRender = 1
+  term = Terminal()
+  hotzoneLocation = [((term.height // 16) + 6), 0]
 
-  # This is the main rendering function for the menu
-  def mainRender(menu, selection):
-    term = Terminal()
-    print(term.clear())
+  def goBack():
+    global selectionInProgress
+    global needsRender
+    selectionInProgress = False
+    needsRender = 1
+    return True
 
-    print(term.clear())
-    print(term.move_y(term.height // 16))
-    print(term.black_on_cornsilk4(term.center('IOTstack Opencpn Commands')))
-    print("")
-    print(term.center(commonTopBorder(renderMode)))
-    print(term.center(commonEmptyLine(renderMode)))
-    print(term.center("{bv}      Select Command to run                                                     {bv}".format(bv=specialChars[renderMode]["borderVertical"])))
+  def enterPortNumberExec():
+    # global term
+    global needsRender
+    global dockerComposeServicesYaml
+    externalPort = getExternalPorts(currentServiceName, dockerComposeServicesYaml)[0]
+    internalPort = getInternalPorts(currentServiceName, dockerComposeServicesYaml)[0]
+    newPortNumber = enterPortNumberWithWhiptail(term, dockerComposeServicesYaml, currentServiceName, hotzoneLocation, externalPort)
 
-    print(term.center(commonEmptyLine(renderMode)))
+    if newPortNumber > 0:
+      dockerComposeServicesYaml[currentServiceName]["ports"][0] = "{newExtPort}:{oldIntPort}".format(
+        newExtPort = newPortNumber,
+        oldIntPort = internalPort
+      )
+      createMenu()
+    needsRender = 1
 
+  def onResize(sig, action):
+    global opencpnBuildOptions
+    global currentMenuItemIndex
+    mainRender(1, opencpnBuildOptions, currentMenuItemIndex)
+
+  opencpnBuildOptions = []
+
+  def createMenu():
+    global opencpnBuildOptions
+    try:
+      opencpnBuildOptions = []
+      portNumber = getExternalPorts(currentServiceName, dockerComposeServicesYaml)[0]
+      opencpnBuildOptions.append([
+        "Change external WUI Port Number from: {port}".format(port=portNumber),
+        enterPortNumberExec
+      ])
+    except: # Error getting port
+      pass
+    opencpnBuildOptions.append(["Go back", goBack])
+
+  def runOptionsMenu():
+    createMenu()
+    menuEntryPoint()
+    return True
+
+  def renderHotZone(term, menu, selection, hotzoneLocation):
     lineLengthAtTextStart = 71
-
+    print(term.move(hotzoneLocation[0], hotzoneLocation[1]))
     for (index, menuItem) in enumerate(menu):
       toPrint = ""
-      if index == selection: # This checks if the current rendering item is the one that's selected
+      if index == selection:
         toPrint += ('{bv} -> {t.blue_on_green} {title} {t.normal} <-'.format(t=term, title=menuItem[0], bv=specialChars[renderMode]["borderVertical"]))
       else:
-        if len(menu[index]) > 2 and "customProperty" in menu[index][2] and menu[index][2]["customProperty"] == True: # A custom property check example
-          toPrint += ('{bv}    {t.black_on_green} {title} {t.normal}   '.format(t=term, title=menuItem[0], bv=specialChars[renderMode]["borderVertical"]))
-        else:
-          toPrint += ('{bv}    {t.normal} {title}    '.format(t=term, title=menuItem[0], bv=specialChars[renderMode]["borderVertical"]))
+        toPrint += ('{bv}    {t.normal} {title}    '.format(t=term, title=menuItem[0], bv=specialChars[renderMode]["borderVertical"]))
 
-      for i in range(lineLengthAtTextStart - len(menuItem[0])): # Pad the remainder of the line
+      for i in range(lineLengthAtTextStart - len(menuItem[0])):
         toPrint += " "
 
       toPrint += "{bv}".format(bv=specialChars[renderMode]["borderVertical"])
 
       toPrint = term.center(toPrint)
-      
+
       print(toPrint)
 
-    print(term.center(commonEmptyLine(renderMode)))
-    if not hideHelpText:
+  def mainRender(needsRender, menu, selection):
+    term = Terminal()
+    
+    if needsRender == 1:
+      print(term.clear())
+      print(term.move_y(term.height // 16))
+      print(term.black_on_cornsilk4(term.center('IOTstack opencpn Options')))
+      print("")
+      print(term.center(commonTopBorder(renderMode)))
       print(term.center(commonEmptyLine(renderMode)))
-      print(term.center("{bv}      Controls:                                                                 {bv}".format(bv=specialChars[renderMode]["borderVertical"])))
-      print(term.center("{bv}      [Up] and [Down] to move selection cursor                                  {bv}".format(bv=specialChars[renderMode]["borderVertical"])))
-      print(term.center("{bv}      [H] Show/hide this text                                                   {bv}".format(bv=specialChars[renderMode]["borderVertical"])))
-      print(term.center("{bv}      [Enter] to run command                                                    {bv}".format(bv=specialChars[renderMode]["borderVertical"])))
-      print(term.center("{bv}      [Escape] to go back to build stack menu                                   {bv}".format(bv=specialChars[renderMode]["borderVertical"])))
+      print(term.center("{bv}      Select Option to configure                                                {bv}".format(bv=specialChars[renderMode]["borderVertical"])))
       print(term.center(commonEmptyLine(renderMode)))
-    print(term.center(commonEmptyLine(renderMode)))
-    print(term.center(commonBottomBorder(renderMode)))
 
+    if needsRender >= 1:
+      renderHotZone(term, menu, selection, hotzoneLocation)
+
+    if needsRender == 1:
+      print(term.center(commonEmptyLine(renderMode)))
+      print(term.center(commonEmptyLine(renderMode)))
+      if not hideHelpText:
+        print(term.center(commonEmptyLine(renderMode)))
+        print(term.center("{bv}      Controls:                                                                 {bv}".format(bv=specialChars[renderMode]["borderVertical"])))
+        print(term.center("{bv}      [Up] and [Down] to move selection cursor                                  {bv}".format(bv=specialChars[renderMode]["borderVertical"])))
+        print(term.center("{bv}      [H] Show/hide this text                                                   {bv}".format(bv=specialChars[renderMode]["borderVertical"])))
+        print(term.center("{bv}      [Enter] to run command or save input                                      {bv}".format(bv=specialChars[renderMode]["borderVertical"])))
+        print(term.center("{bv}      [Escape] to go back to build stack menu                                   {bv}".format(bv=specialChars[renderMode]["borderVertical"])))
+        print(term.center(commonEmptyLine(renderMode)))
+        if len(documentationHint) > 1:
+          if len(documentationHint) > 56:
+            documentationAndPadding = padText(documentationHint, 71)
+            print(term.center("{bv}      Documentation:                                                            {bv}".format(bv=specialChars[renderMode]["borderVertical"])))
+            print(term.center("{bv}        {dap} {bv}".format(bv=specialChars[renderMode]["borderVertical"], dap=documentationAndPadding)))
+          else:
+            documentationAndPadding = padText(documentationHint, 56)
+            print(term.center("{bv}        Documentation: {dap} {bv}".format(bv=specialChars[renderMode]["borderVertical"], dap=documentationAndPadding)))
+          print(term.center(commonEmptyLine(renderMode)))
+      print(term.center(commonEmptyLine(renderMode)))
+      print(term.center(commonBottomBorder(renderMode)))
 
   def runSelection(selection):
-    term = Terminal()
-    if len(menuItemsExample[selection]) > 1 and isinstance(menuItemsExample[selection][1], types.FunctionType):
-      menuItemsExample[selection][1]()
+    import types
+    global opencpnBuildOptions
+    if len(opencpnBuildOptions[selection]) > 1 and isinstance(opencpnBuildOptions[selection][1], types.FunctionType):
+      opencpnBuildOptions[selection][1]()
     else:
-      print(term.green_reverse('IOTstack Opencpn Error: No function assigned to menu item: "{}"'.format(menuItemsExample[selection][0])))
+      print(term.green_reverse('IOTstack Error: No function assigned to menu item: "{}"'.format(nodeRedBuildOptions[selection][0])))
 
   def isMenuItemSelectable(menu, index):
     if len(menu) > index:
       if len(menu[index]) > 2:
-        if "skip" in menu[index][2] and menu[index][2]["skip"] == True:
+        if menu[index][2]["skip"] == True:
           return False
     return True
 
@@ -276,18 +240,19 @@ def main():
     global menuNavigateDirection
     global needsRender
     global hideHelpText
+    global opencpnBuildOptions
     term = Terminal()
     with term.fullscreen():
       menuNavigateDirection = 0
-      mainRender(menuItemsExample, currentMenuItemIndex)
+      mainRender(needsRender, opencpnBuildOptions, currentMenuItemIndex)
       selectionInProgress = True
       with term.cbreak():
         while selectionInProgress:
           menuNavigateDirection = 0
 
           if needsRender: # Only rerender when changed to prevent flickering
-            mainRender(menuItemsExample, currentMenuItemIndex)
-            needsRender = False
+            mainRender(needsRender, opencpnBuildOptions, currentMenuItemIndex)
+            needsRender = 0
 
           key = term.inkey(esc_delay=0.05)
           if key.is_sequence:
@@ -309,24 +274,22 @@ def main():
                 hideHelpText = False
               else:
                 hideHelpText = True
-              mainRender(1, menuItemsExample, currentMenuItemIndex)
+              mainRender(1, opencpnBuildOptions, currentMenuItemIndex)
 
           if menuNavigateDirection != 0: # If a direction was pressed, find next selectable item
             currentMenuItemIndex += menuNavigateDirection
-            currentMenuItemIndex = currentMenuItemIndex % len(menuItemsExample)
-            needsRender = True
+            currentMenuItemIndex = currentMenuItemIndex % len(opencpnBuildOptions)
+            needsRender = 2
 
-            while not isMenuItemSelectable(menuItemsExample, currentMenuItemIndex):
+            while not isMenuItemSelectable(opencpnBuildOptions, currentMenuItemIndex):
               currentMenuItemIndex += menuNavigateDirection
-              currentMenuItemIndex = currentMenuItemIndex % len(menuItemsExample)
+              currentMenuItemIndex = currentMenuItemIndex % len(opencpnBuildOptions)
     return True
 
+  ####################
+  # End menu section
+  ####################
 
-
-
-
-  # Entrypoint for execution
->>>>>>> e248047 (Create build.py)
   if haltOnErrors:
     eval(toRun)()
   else:
@@ -337,14 +300,7 @@ def main():
 
 # This check isn't required, but placed here for debugging purposes
 global currentServiceName # Name of the current service
-<<<<<<< HEAD
-if currentServiceName == 'opencpn': # --- Ensure to update the service name with yours
-  main()
-else:
-  print("Error. '{}' Tried to run 'opencpn' config".format(currentServiceName)) # --- Ensure to update the service name with yours
-=======
 if currentServiceName == 'opencpn':
   main()
 else:
   print("Error. '{}' Tried to run 'opencpn' config".format(currentServiceName))
->>>>>>> e248047 (Create build.py)
